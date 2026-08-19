2faeea0e957fdd8ba21a1fbd76f5784001237936
MahamedM19 <mahamedmohamed5675@gmail.com>
2026-08-19 19:14:56 +0100

    Count failed login attempts in security log

--- log_analyser.py
+++ log_analyser.py
@@ -1,3 +1,8 @@
+failed_logins = 0
+
 with open("security.log", "r") as file:
     for line in file:
-        print(line.strip())
+        if "LOGIN_FAILED" in line:
+            failed_logins += 1
+
+print("There has been", failed_logins, "failed login attempts")
