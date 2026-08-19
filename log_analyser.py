e1d4b00e3fa9f1bea9ff03a52793f1e0b8ed8a05
MahamedM19 <mahamedmohamed5675@gmail.com>
2026-08-19 19:19:01 +0100

    Add IP tracking for failed login attempts
    Track failed login attempts and count by IP address.

--- log_analyser.py
+++ log_analyser.py
@@ -1,8 +1,22 @@
 failed_logins = 0
+failed_ips = {}
 
 with open("security.log", "r") as file:
     for line in file:
         if "LOGIN_FAILED" in line:
             failed_logins += 1
 
+            parts = line.split()
+            ip_address = parts[4].replace("ip=", "")
+
+            if ip_address in failed_ips:
+                failed_ips[ip_address] += 1
+            else:
+                failed_ips[ip_address] = 1
+
 print("There has been", failed_logins, "failed login attempts")
+
+print("\nFailed logins by IP:")
+
+for ip, count in failed_ips.items():
+    print(ip, ":", count)
